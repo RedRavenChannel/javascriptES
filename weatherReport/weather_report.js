0a1a21cac4e76b5ce1941abc82e4bcf573ee7fc4
Lance Clark <Lance1718@gmail.com>
2024-06-20 06:58:52 -0400

    added lat long

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -12,7 +12,6 @@ function showweatherDetails(event) {
     const apiKey = '524e2fe3dd4e46e9c843f62bf1cb280b'; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
@@ -28,5 +27,31 @@ function showweatherDetails(event) {
     weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
   });
 }
+function showweatherDetailsByLatLong(event) {
+    event.preventDefault();
 
- document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+
+    const apiKey = '524e2fe3dd4e46e9c843f62bf1cb280b'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrlLatLong = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={API key}`;
+
+    fetch(apiUrlLatLong)
+    .then(response => response.json())
+    .then(data => {
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                            <p>Temperature: ${data.main.temp} &#8451;</p>
+                            <p>Weather: ${data.weather[0].description}</p>`;
+                            
+})
+.catch(error => {
+    console.error('Error fetching weather:', error);
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+  });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails);
+ document.getElementById('weatherForm').addEventListener('submitLatLong',showweatherDetailsByLatLong );
